3e16b2fb2df5ff1874c38edebef3d6e6bff34709
cornelber <bercornel@gmail.com>
2024-10-16 21:37:29 +0300

    implemented feature to view all students to table format

--- src/ro/ubb/studentlabapp/Repository/ICRUDRepository.java
+++ src/ro/ubb/studentlabapp/Repository/ICRUDRepository.java
@@ -1,5 +1,8 @@
 package ro.ubb.studentlabapp.Repository;
 
+import java.util.List;
+
 public interface ICRUDRepository<Entity> {
     boolean save(Entity entity);
+    List<Entity> findAll();
 }
--- src/ro/ubb/studentlabapp/Service/Impl/StudentService.java
+++ src/ro/ubb/studentlabapp/Service/Impl/StudentService.java
@@ -4,11 +4,18 @@ import ro.ubb.studentlabapp.Domain.Student;
 import ro.ubb.studentlabapp.Repository.ICRUDRepository;
 import ro.ubb.studentlabapp.Service.IStudentService;
 
+import java.util.List;
+
 public class StudentService implements IStudentService {
     private ICRUDRepository<Student> studentRepository;
 
     public StudentService(ICRUDRepository<Student> studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return studentRepository.findAll();
     }
 
     @Override
